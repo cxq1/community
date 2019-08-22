8e2065fc726b361b1853e49f6ed8581bbdddb720
cxq <1986541495@qq.com>
2019-08-22 08:46:53 +0800

    update question

--- src/main/java/com/cxq/community/community/Controller/PulishController.java
+++ src/main/java/com/cxq/community/community/Controller/PulishController.java
@@ -1,13 +1,16 @@
 package com.cxq.community.community.Controller;
 
+import com.cxq.community.community.dto.QuestionDTO;
 import com.cxq.community.community.mapper.QuestionMapper;
 import com.cxq.community.community.mapper.UserMapper;
 import com.cxq.community.community.model.Question;
 import com.cxq.community.community.model.User;
+import com.cxq.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,11 +22,22 @@ public class PulishController {
 
     @Autowired
     private UserMapper userMapper;
+
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
 
     @GetMapping("/publish")
     public String pulish(){
+        return "/publish";
+    }
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable("id") Integer id ,Model model){
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title",question.getTitle());
+        model.addAttribute("description",question.getDescription());
+        model.addAttribute("tag",question.getTag());
+        model.addAttribute("id",question.getId());
         return "/publish";
     }
 
@@ -31,6 +45,7 @@ public class PulishController {
     public String doPublish(@RequestParam(value = "title",required = false)String title,
                             @RequestParam(value = "description",required = false)String description,
                             @RequestParam(value = "tag",required = false)String tag,
+                            @RequestParam(value = "id",required = false)Integer id,
                             HttpServletRequest request, Model model
                             ){
         model.addAttribute("title",title);
@@ -59,9 +74,8 @@ public class PulishController {
         question.setDescription(description);
         question.setCreator(user.getId());
         question.setTag(tag);
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-        questionMapper.create(question);
+        question.setId(id);
+        questionService.createOrUpdate(question);
         return "redirect:/";
     }
 }
