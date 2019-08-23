40111ea17e93a873016f79cb635b9f16ef346752
cxq <1986541495@qq.com>
2019-08-23 14:01:23 +0800

    不刷新页面登录

--- src/main/java/com/cxq/community/community/service/CommentService.java
+++ src/main/java/com/cxq/community/community/service/CommentService.java
@@ -10,6 +10,7 @@ import com.cxq.community.community.model.Comment;
 import com.cxq.community.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentService {
@@ -19,7 +20,7 @@ public class CommentService {
     private QuestionMapper questionMapper;
     @Autowired
     private QuestionExtMapper questionExtMapper;
-
+    @Transactional
     public void insert(Comment comment) {
         if(comment.getParentId()==null||comment.getParentId()==0){
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
--- src/main/java/com/cxq/community/community/service/QuestionService.java
+++ src/main/java/com/cxq/community/community/service/QuestionService.java
@@ -129,10 +129,23 @@ public class QuestionService {
         if(question.getId()==null){
             question.setGmtCreate(System.currentTimeMillis());
             question.setGmtModified(question.getGmtCreate());
+            question.setViewCount(0);
+            question.setLikeCount(0);
+            question.setCommentCount(0);
             questionMapper.insert(question);
         }else {
-            question.setGmtModified(question.getGmtCreate());
-            questionMapper.updateByPrimaryKeySelective(question);
+            Question updateQuestion = new Question();
+            updateQuestion.setGmtModified(System.currentTimeMillis());
+            updateQuestion.setTitle(question.getTitle());
+            updateQuestion.setDescription(question.getDescription());
+            updateQuestion.setTag(question.getTag());
+            QuestionExample example = new QuestionExample();
+            example.createCriteria()
+                    .andIdEqualTo(question.getId());
+            int updated = questionMapper.updateByExampleSelective(updateQuestion, example);
+            if (updated != 1) {
+                throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
+            }
         }
     }
 
