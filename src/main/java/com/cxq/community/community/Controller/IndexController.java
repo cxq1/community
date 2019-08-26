fcfa20139dedc062d91f3e58adff403d9b7374e7
cxq <1986541495@qq.com>
2019-08-26 12:00:27 +0800

    add controller debug

--- src/main/java/com/cxq/community/community/Controller/IndexController.java
+++ src/main/java/com/cxq/community/community/Controller/IndexController.java
@@ -31,7 +31,6 @@ public class IndexController {
                         @RequestParam(name = "search", required = false) String search) {
         PaginationDTO pagination = questionService.list(search, page, size);
         model.addAttribute("pagination", pagination);
-        System.out.println("pagination:"+pagination);
         model.addAttribute("search", search);
         return "index";
     }
