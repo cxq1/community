5501550a3ce2346dffec25b3545fd60e18dd45e6
cxq <1986541495@qq.com>
2019-08-26 11:18:33 +0800

    fix /

--- src/main/java/com/cxq/community/community/Controller/PulishController.java
+++ src/main/java/com/cxq/community/community/Controller/PulishController.java
@@ -15,43 +15,42 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class PulishController {
 
     @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
     private QuestionService questionService;
 
-    @GetMapping("/publish")
-    public String pulish(Model model){
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Long id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
         model.addAttribute("tags", TagCache.get());
-        return "/publish";
+        return "publish";
     }
 
-    @GetMapping("/publish/{id}")
-    public String edit(@PathVariable("id") Long id ,Model model){
-        QuestionDTO question = questionService.getById(id);
-        model.addAttribute("title",question.getTitle());
-        model.addAttribute("description",question.getDescription());
-        model.addAttribute("tag",question.getTag());
-        model.addAttribute("id",question.getId());
+
+    @GetMapping("/publish")
+    public String publish(Model model) {
         model.addAttribute("tags", TagCache.get());
-        return "/publish";
+        return "publish";
     }
 
     @PostMapping("/publish")
-    public String doPublish(@RequestParam(value = "title",required = false)String title,
-                            @RequestParam(value = "description",required = false)String description,
-                            @RequestParam(value = "tag",required = false)String tag,
-                            @RequestParam(value = "id",required = false)Long id,
-                            HttpServletRequest request, Model model
-                            ){
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
+    public String doPublish(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Long id,
+            HttpServletRequest request,
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
         model.addAttribute("tags", TagCache.get());
 
         if (title == null || title == "") {
@@ -66,22 +65,24 @@ public class PulishController {
             model.addAttribute("error", "标签不能为空");
             return "publish";
         }
+
         String invalid = TagCache.filterInvalid(tag);
         if (StringUtils.isNotBlank(invalid)) {
             model.addAttribute("error", "输入非法标签:" + invalid);
             return "publish";
         }
-        User user = (User) request.getSession().getAttribute("user");
 
-        if(user==null){
-            model.addAttribute("error","用户未登录");
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
             return "publish";
         }
+
         Question question = new Question();
         question.setTitle(title);
         question.setDescription(description);
-        question.setCreator(user.getId());
         question.setTag(tag);
+        question.setCreator(user.getId());
         question.setId(id);
         questionService.createOrUpdate(question);
         return "redirect:/";
